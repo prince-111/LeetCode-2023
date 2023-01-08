32e0f996e767537e89dd41a163b2169bc7950b79
Prince Kumar <88477312+prince-111@users.noreply.github.com>
2023-01-08 11:46:23 +0000

    🚀 Detect Capital using string

--- January 2023/day-2.cpp
+++ January 2023/day-2.cpp
@@ -1,6 +1,7 @@
 /*
 520. Detect Capital
 https://leetcode.com/problems/detect-capital/
+
 We define the usage of capitals in a word to be right when one of the following cases holds:
 All letters in this word are capitals, like "USA".
 All letters in this word are not capitals, like "leetcode".
